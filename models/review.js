8963a0decaaf8440a8f2aac59c7900f90d673610
STM <startinmerc@gmail.com>
2020-02-14 15:07:22 +0100

    link User to Review

--- models/review.js
+++ models/review.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const User = require("./user");
 
 const reviewSchema = new mongoose.Schema({
 	title: {
@@ -9,9 +10,9 @@ const reviewSchema = new mongoose.Schema({
 		type: Number,
 		required: true,
 	},
-	author: {
+	user: {
 		type: mongoose.Schema.Types.ObjectId,
-		ref: "Author"
+		ref: "User"
 	},
 	content: {
 		type: String,
