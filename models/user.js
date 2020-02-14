37c4d7379475c13bbf228a28c5de3f3afc08e7e3
STM <startinmerc@gmail.com>
2020-02-14 12:31:48 +0100

    comments!

--- models/user.js
+++ models/user.js
@@ -18,24 +18,36 @@ const userSchema = new mongoose.Schema({
 	},
 });
 
+// Before User is saved
 userSchema.pre("save", async function(){
 	try {
+		// If password hasn't changed
 		if(!this.isModified("password")){
+			// Skip
 			return next();
+		// Otherwise
 		} else {
+			// Encrypt password using async brypt.hash, provide salt key of 10
 			let hashedPassword = await bcrypt.hash(this.password, 10);
+			// Replace plain with encrypted password
 			this.password = hashedPassword;
+			// Carry on
 			return next();
 		}
+	// Catch & pass on errors
 	} catch(err) {
 		return next(err);
 	}
 });
 
+// Add method to schema for password checking
 userSchema.method.comparePassword = async function(candidatePassword, next){
 	try {
+		// Compare submitted & stored passwords using async bcrypt.compare
 		let isMatch = await bcrypt.compare(candidatePassword, this.password);
+		// Return boolean
 		return isMatch;
+	// Catch & pass on errors
 	} catch(err) {
 		return next(err);
 	}
