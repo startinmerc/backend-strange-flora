b0045327bc5eac7048b24cbf7ae02e66ed0f2b2d
STM <startinmerc@gmail.com>
2020-02-14 16:37:41 +0100

    add get & delete to routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
 
-const { createReview } = require("../handlers/reviews");
+const { createReview, getReview, deleteReview } = require("../handlers/reviews");
 
 router.route("/").post(createReview);
+
+router.route("/:review_id").get(getReview);
+router.route("/:review_id").delete(deleteReview);
 
 module.exports = router;
