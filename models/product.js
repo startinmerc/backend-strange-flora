542ab6bb93e001e4025e8e72a31155b498b700dd
STM <startinmerc@gmail.com>
2020-02-14 11:48:12 +0100

    Add Product model

---
+++ models/product.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const productSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	photos: {
+		type: Array,
+		required: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	reviews: {
+		type: Array,
+		required: false
+	},
+	price: {
+		type: String,
+		required: true
+	},
+	type: {
+		type: String,
+		required: true
+	},
+	stock: {
+		type: Number,
+		required: true
+	}
+});
+
+const Product = mongoose.Model("Product", productSchema);
+
+module.exports = Product;
