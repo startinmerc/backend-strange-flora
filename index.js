9baa9739c2d59319f4fd6b8123f288c3f91ba3bb
STM <startinmerc@gmail.com>
2020-02-14 11:06:27 +0100

    add basic 404

--- index.js
+++ index.js
@@ -7,12 +7,17 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const errorHandler = require("./handlers/error");
 
-
 // Express Config
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Error Handler
+app.use((req,res,next)=>{
+	let err = new Error("Not Found");
+	err.status = 404;
+	next(err);
+});
 
 // Start App
 app.listen(PORT, function(){
