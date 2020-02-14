b575d949ffd22bc7ee61b382c9a2e8239158fb0f
STM <startinmerc@gmail.com>
2020-02-14 15:33:16 +0100

    Add createReview handler

---
+++ handlers/reviews.js
@@ -0,0 +1,44 @@
+const db = require("../models");
+
+exports.createReview = async function(req,res,next){
+	try {
+		// create Review with supplied data
+		let review = await db.Review.create({
+			title: req.body.title,
+			score: req.body.score,
+			content: req.body.content,
+			user: req.params.id
+		});
+		// Find author by id
+		let foundUser = await db.User.findById(req.params.id);
+		// Add review to author's reviews array
+		foundUser.reviews.push(review.id);
+		// Wait for db to save
+		await foundUser.save();
+		// Add username to review response to save re-looking up
+		let foundReview = await db.Review.findById(review_id).populate("user", {
+			username: true
+		});
+		// Reply with new review
+		return res.status(200).json(foundReview);
+	// Catch errors
+	} catch(err) {
+		return next(err);
+	}
+};
+
+exports.getReview = async function(req,res,next){
+	try {
+
+	} catch(err) {
+		return next(err);
+	}
+};
+
+exports.deleteReview = async function(req,res,next){
+	try {
+
+	} catch(err) {
+		return next(err);
+	}
+};
