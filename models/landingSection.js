2f48594bae5099919367896367ae8a74c3579ee8
STM <startinmerc@gmail.com>
2020-02-14 11:58:39 +0100

    Add LandingSection model

---
+++ models/landingSection.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const landingSectionSchema = new mongoose.Schema({
+	dark: {
+		type: Boolean,
+		required: true,
+		default: false
+	},
+	header: {
+		type: String,
+		required: true
+	},
+	copy: {
+		type: String,
+		required: true
+	},
+	image: {
+		type: String,
+		required: true
+	},
+	links: {
+		type: Array,
+		required: true
+	},
+	type: {
+		type: String,
+		required: true
+	}
+});
+
+const LandingSection = mongoose.Model("LandingSection", landingSectionSchema);
+
+module.exports = LandingSection;
