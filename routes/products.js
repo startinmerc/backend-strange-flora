e6c70e8b01acc2435d5a7b63e72af05d7297dec3
STM <startinmerc@gmail.com>
2020-02-19 13:31:31 +0100

    tidy code

--- routes/products.js
+++ routes/products.js
@@ -4,10 +4,7 @@ const router = express.Router({mergeParams: true});
 const { createProduct, getProduct, getAllProducts } = require("../handlers/products");
 
 router.route("/all").get(getAllProducts);
-
 router.route("/").post(createProduct);
-
 router.route("/:product_id").get(getProduct);
-// router.route("/:review_id").delete(deleteProduct);
 
 module.exports = router;
