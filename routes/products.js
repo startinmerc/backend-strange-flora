68a522f6ec07d53b2221ab1badcba8c38c14dfe4
STM <startinmerc@gmail.com>
2020-04-28 22:08:30 +0100

    getFeaturedProducts route

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router({mergeParams: true});
 
-const { createProduct, getProduct, getAllProducts } = require("../handlers/products");
+const { createProduct, getProduct, getAllProducts, getFeaturedProducts } = require("../handlers/products");
 
 router.route("/all").get(getAllProducts);
 router.route("/").post(createProduct);
+router.route("/featured/:category").get(getFeaturedProducts);
 router.route("/:product_id").get(getProduct);
 
 module.exports = router;
