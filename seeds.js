a615011caeb4be3370189ee8084be4c0832c02c4
STM <startinmerc@gmail.com>
2020-06-01 14:18:28 +0100

    Fix seeding

--- seeds.js
+++ seeds.js
@@ -51,51 +51,68 @@ function addDelivery({title, price, description, speed}){
 };
 
 function seedDB(){
-	Product.deleteMany({});
-	LandingSection.deleteMany({});
-	Category.deleteMany({});
-	Delivery.deleteMany({});
-
-	categories.forEach((seed)=>{
-		Category.create({
-			title: seed.title,
-			section: seed.section,
-			color: seed.color
-		});
+	Product.deleteMany({},err=>{
+		if(err){
+			console.log(err)
+		} else {
+			for(let i=0;i<40;i++){
+				addProduct(i);
+			};
+		}
 	});
 
-	deliveries.forEach(seed=>{
-		addDelivery(seed)
-	})
+	LandingSection.deleteMany({},err=>{
+		if(err){
+			console.log(err)
+		} else {
+			addToLanding(
+				false,
+				'Big Mushroom Offer',
+				'Buy seven mushrooms get one free! Limited stock available.',
+				'https://images.unsplash.com/photo-1528518290605-1fcc8dcca204?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+				'mushroom',
+				[{title: 'Shop Now', link: '/products/mushrooms'},{title: 'Shop Later', link: '/products/mushrooms'}]
+			);
+			addToLanding(
+				true,
+				'Berry Special',
+				"See what we did there? It's a pun. You like puns, don't you? Yeah you do.",
+				'https://images.unsplash.com/photo-1445197138520-6099f1c07aa0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+				'berry',
+				[{title: 'Shop Now', link: '/products/berries'},{title: 'Shop Later', link: '/products/berries'}]
+			);
+			addToLanding(
+				false,
+				'Lovely Lovely Flowers',
+				"Lovely flowers. Just don't eat them. Really, don't eat them.",
+				'https://images.unsplash.com/photo-1567748157439-651aca2ff064?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
+				'flower',
+				[{title: 'Shop Now', link: '/products/flowers'},{title: 'Shop Later', link: '/products/flowers'}]
+			);
+		}
+	});
 
-	for(let i=0;i<40;i++){
-		addProduct(i);
-	};
+	Category.deleteMany({},err=>{
+		if(err){
+			console.log(err)
+		} else {
+			categories.forEach((seed)=>{
+				Category.create({
+					title: seed.title,
+					section: seed.section,
+					color: seed.color
+				});
+			});
+		}});
 
-	addToLanding(
-		false,
-		'Big Mushroom Offer',
-		'Buy seven mushrooms get one free! Limited stock available.',
-		'https://images.unsplash.com/photo-1528518290605-1fcc8dcca204?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
-		'mushroom',
-		[{title: 'Shop Now', link: '/products/mushrooms'},{title: 'Shop Later', link: '/products/mushrooms'}]
-	);
-	addToLanding(
-		true,
-		'Berry Special',
-		"See what we did there? It's a pun. You like puns, don't you? Yeah you do.",
-		'https://images.unsplash.com/photo-1445197138520-6099f1c07aa0?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
-		'berry',
-		[{title: 'Shop Now', link: '/products/berries'},{title: 'Shop Later', link: '/products/berries'}]
-	);
-	addToLanding(
-		false,
-		'Lovely Lovely Flowers',
-		"Lovely flowers. Just don't eat them. Really, don't eat them.",
-		'https://images.unsplash.com/photo-1567748157439-651aca2ff064?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9',
-		'flower',
-		[{title: 'Shop Now', link: '/products/flowers'},{title: 'Shop Later', link: '/products/flowers'}]
-	);
+	Delivery.deleteMany({},err=>{
+		if(err){
+			console.log(err)
+		} else {
+			deliveries.forEach(seed=>{
+				addDelivery(seed)
+			})
+		}});
 }
 
 module.exports = seedDB;
