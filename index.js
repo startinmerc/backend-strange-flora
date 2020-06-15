f9ca9e8dd05cb357392a5451e7287765b4f016c3
STM <startinmerc@gmail.com>
2020-06-15 10:50:49 +0100

    Seeded

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ app.use(errorHandler);
 
 // ===================Start App===================
 
-const seedDB = require("./seeds");
-seedDB();
+// const seedDB = require("./seeds");
+// seedDB();
 
 app.listen(PORT, function(){
 	console.log(`Server running on port ${PORT}`);
