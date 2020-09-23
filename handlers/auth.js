a491992cdfc0f39267d911c3ebec126917548832
STM <startinmerc@gmail.com>
2020-09-23 12:13:16 +0200

    Better docs in auth handler

--- handlers/auth.js
+++ handlers/auth.js
@@ -1,74 +1,76 @@
 const db = require("../models");
 const jwt = require("jsonwebtoken");
 
-exports.signin = async function(req,res,next){
+exports.signin = async function (req, res, next) {
 	try {
-		// Find user in DB
+		// Find user by email from req.body
 		let user = await db.User.findOne({
-				email: req.body.email
-			});
-		// Assign id & username
+			email: req.body.email
+		});
+		// Deconstruct user data to variables
 		let { id, username } = user;
-		// Check password is correct
+		// Check password via middleware
 		let isMatch = await user.comparePassword(req.body.password);
-		// If so
-		if(isMatch) {
-			// Set JSON web token with id & username
+		// If password correct
+		if (isMatch) {
+			// Create JSON web token for user
 			let token = jwt.sign({
-					id,
-					username
-				}, process.env.SECRET_KEY
+				id,
+				username
+			}, process.env.SECRET_KEY
 			);
-			// Return OK status with JWT
+			// Return user data + auth token
 			return res.status(200).json({
 				id,
 				username,
 				token
 			});
-		// Otherwise return invalid email/password
+			// Catch errors
 		} else {
 			return next({
 				status: 400,
-				message: "Invalid Email/Password"
-			});
-		};
-	// Catch non-existing user
-	} catch(err) {
-			return next({
-				status: 400,
-				message: "Invalid Email/Password"
-			});
-	};
+				message: "Invalid email/password"
+			})
+		}
+		// Catch errors
+	} catch (err) {
+		return next({
+			status: 400,
+			message: "Invalid email/password"
+		});
+	}
 };
 
-exports.signup = async function(req,res,next){
+exports.signup = async function (req, res, next) {
 	try {
-		// Create User from schema
+		// Create new User from req.body
 		let user = await db.User.create(req.body);
-		// Destructure id + username
-		let { id, username } = user;
-		// Create JSON web token w/ these + secret key encryption
-		let token = jwt.sign({
-			id,
-			username
-		}, process.env.SECRET_KEY
+		// Deconstruct user data to variables
+		let { id, username,  } = user;
+		// Create JSON web token for user
+		let token = jwt.sign(
+			{
+				id,
+				username
+			},
+			process.env.SECRET_KEY
 		);
-		// Return next w/ JSON of user data + token
+		// Return user data + auth token
 		return res.status(200).json({
 			id,
 			username,
 			token
 		});
-	} catch(err) {
-		// If validation fails (non-unique)
-		if(err.code === 11000){
-			// Reformat error message
+		// Catch errors
+	} catch (err) {
+		// If user taken reply with custom message
+		if (err.code === 11000) {
 			err.message = "Sorry, that username and/or email is taken";
 		}
-		// Return next with error
+		// Return error & status
 		return next({
 			status: 400,
 			message: err.message
 		});
-	};
+	}
 };
